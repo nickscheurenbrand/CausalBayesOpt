e070165a569f4d98dfdb259e8f9d27238483c69f
Nicholas Scheurenbrand <nps25@ic.ac.uk>
2026-07-15 14:28:02 +0100

    results boundary

--- job_cold_start.sh
+++ job_cold_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=4:mem=16gb
-#PBS -l walltime=02:00:00
+#PBS -l select=1:ncpus=4:mem=16gb:ngpus=1
+#PBS -l walltime=20:00:00
 #PBS -N cold_start_bootstrap_dump
 
 source $HOME/causal_bayes_opt/.venv/bin/activate
